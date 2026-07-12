3ae0bd11b31713e74cca1ddc9005667e69e702a2
Visweshvar <visweshvar.ust@gmail.com>
2026-07-12 16:26:46 +0530

    Create findMiddleNode_LinkedList.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

---
+++ src/DSA/S6/findMiddleNode_LinkedList.java
@@ -0,0 +1,95 @@
+package DSA.S6;
+
+public class findMiddleNode_LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public findMiddleNode_LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (head == null) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("\nLinked List:");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public Node f(){
+//        if (head == null) return null; check about edge case. if not needed
+        Node fast = head;
+        Node slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public Node findMiddleNode(){
+        if(head == null) return null;
+        else if(head.next == null) return head;
+        Node p1 = head;
+        Node p2 = head;
+        while(p2 != null && p2.next != null){
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        return p1;
+    }
+
+}
+
