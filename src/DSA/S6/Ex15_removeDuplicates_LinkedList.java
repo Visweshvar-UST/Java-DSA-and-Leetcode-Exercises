b13ac03da2122a24d3bf97427c9a603a23f5efb3
Visweshvar <visweshvar.ust@gmail.com>
2026-07-14 19:24:51 +0530

    Create Ex15_removeDuplicates_LinkedList.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

---
+++ src/DSA/S6/Ex15_removeDuplicates_LinkedList.java
@@ -0,0 +1,94 @@
+package DSA.S6;
+
+
+public class Ex15_removeDuplicates_LinkedList {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Ex15_removeDuplicates_LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    // WRITE THE REMOVEDUPLICATES METHOD HERE //
+    // own passed in one run
+    public void removeDuplicates() {
+        if(head == null || head.next == null) return;
+        Node curr = head;
+        Node run = head;
+        while(curr != null){
+            while(run.next != null){
+                if(run.next.value == curr.value) {
+                    run.next = run.next.next;
+                    continue;
+                }
+                run = run.next;
+            }
+            curr = curr.next;
+            run = curr;
+        }
+    }
+
+}
+
