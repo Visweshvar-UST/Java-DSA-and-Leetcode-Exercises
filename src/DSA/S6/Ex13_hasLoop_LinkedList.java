09d153ba4bed58db96df8aec3853650bd9c2203a
Visweshvar <visweshvar.ust@gmail.com>
2026-07-15 16:45:54 +0530

    Update Ex13_hasLoop_LinkedList.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

--- src/DSA/S6/Ex13_hasLoop_LinkedList.java
+++ src/DSA/S6/Ex13_hasLoop_LinkedList.java
@@ -81,5 +81,30 @@ public class Ex13_hasLoop_LinkedList {
         return false;
     }
 
+//    /**
+//     * Definition for singly-linked list.
+//     * class ListNode {
+//     *     int val;
+//     *     ListNode next;
+//     *     ListNode(int x) {
+//     *         val = x;
+//     *         next = null;
+//     *     }
+//     * }
+//     */
+//    https://leetcode.com/problems/linked-list-cycle/description/
+//    public class Solution {
+//        public boolean hasCycle(ListNode head) {
+//            ListNode slow = head;
+//            ListNode fast = head;
+//            while(fast != null && fast.next != null){
+//                slow = slow.next;
+//                fast = fast.next.next;
+//                if(slow == fast) return true;
+//            }
+//            return false;
+//        }
+//    }
+
 }
 
