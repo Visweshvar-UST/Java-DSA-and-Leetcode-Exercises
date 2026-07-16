80def934489744c8450ab44f23e77069433576d9
Visweshvar <visweshvar.ust@gmail.com>
2026-07-16 17:11:30 +0530

    Create LinkedList.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

---
+++ src/DSA/S6/LinkedList.java
@@ -0,0 +1,92 @@
+package DSA.S6;
+
+public class Ex18_reverseBetweenOwn_LinkedList {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Ex18_reverseBetweenOwn_LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.value);
+                temp = temp.next;
+                if (temp != null) {
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+    
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    // WRITE THE REVERSEBETWEEN METHOD HERE //
+    //                                      //
+    //                                      //
+    //                                      //
+    //                                      //
+    //////////////////////////////////////////
+    public void reverseBetweenOwn(int startIndex, int endIndex){
+        if(startIndex < 0 || endIndex >= length) return;
+        int iter = endIndex - startIndex;
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node pre = dummy;
+        for(int i = 0; i < startIndex; i++){
+            pre = pre.next;
+        }
+        Node curr = pre.next;
+        for(int i = 0; i < iter; i++){
+            Node moveTo = curr.next;
+            curr.next = moveTo.next;
+            moveTo.next = pre.next;
+            pre.next = moveTo;
+        }
+        head = dummy.next;
+        dummy.next = null;
+    }
+
+}
