8c542efbcf25b2541337dea5d20807c221e8e88e
Visweshvar <visweshvar.ust@gmail.com>
2026-07-17 13:44:56 +0530

    Update Ex19_swapPairs_LinkedList.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

--- src/DSA/S6/Ex19_swapPairs_LinkedList.java
+++ src/DSA/S6/Ex19_swapPairs_LinkedList.java
@@ -114,4 +114,34 @@ public class Ex19_swapPairs_LinkedList {
 
         head = dummy.next;
     }
+
+//
+//    /**
+//     * Definition for singly-linked list.
+//     * public class ListNode {
+//     *     int val;
+//     *     ListNode next;
+//     *     ListNode() {}
+//     *     ListNode(int val) { this.val = val; }
+//     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//     * }
+//     */
+//    https://leetcode.com/problems/swap-nodes-in-pairs/
+//    class Solution {
+//        public ListNode swapPairs(ListNode head) {
+//            ListNode dummy = new ListNode(0);
+//            dummy.next = head;
+//            ListNode pre = dummy;
+//            ListNode first = head;
+//            while(first != null && first.next != null){
+//                ListNode second = first.next;
+//                first.next = second.next;
+//                second.next = first;
+//                pre.next = second;
+//                pre = first;
+//                first = pre.next;
+//            }
+//            return dummy.next;
+//        }
+//    }
 }
