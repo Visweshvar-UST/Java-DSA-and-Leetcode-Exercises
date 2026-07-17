88bc92d8e518ba9cd17864b9251281d7994fbfe5
Visweshvar <visweshvar.ust@gmail.com>
2026-07-17 15:06:48 +0530

    Create Main.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

---
+++ src/DSA/S7/Main.java
@@ -0,0 +1,13 @@
+package DSA.S7;
+
+public class Main {
+    public static void main(String[] args){
+        DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(5);
+
+        System.out.println(myDoublyLinkedList.getHead().value);
+        System.out.println(myDoublyLinkedList.getTail().value);
+        System.out.println(myDoublyLinkedList.getLength());
+
+        myDoublyLinkedList.printList();
+    }
+}
