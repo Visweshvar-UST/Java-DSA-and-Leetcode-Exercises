c5d54c88b3345c0c866b2a473efa5e54f400e8ce
Visweshvar <visweshvar.ust@gmail.com>
2026-07-23 13:56:24 +0530

    Update Main.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

--- src/DSA/S7/Main.java
+++ src/DSA/S7/Main.java
@@ -17,6 +17,7 @@ public class Main {
         System.out.println(myDoublyLinkedList.get(1));
 
         myDoublyLinkedList.set(0,10);
+        
         System.out.println();
 
         myDoublyLinkedList.printList();
