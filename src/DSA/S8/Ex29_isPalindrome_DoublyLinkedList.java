955d98f1c2f9b2d8b3749de28865a21091a52108
Visweshvar <visweshvar.ust@gmail.com>
2026-07-23 16:29:55 +0530

    Create Ex29_isPalindrome_DoublyLinkedList.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

---
+++ src/DSA/S8/Ex29_isPalindrome_DoublyLinkedList.java
@@ -0,0 +1,101 @@
+package DSA.S8;
+
+public class Ex29_isPalindrome_DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Ex29_isPalindrome_DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nDoubly Linked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+    
+    public void append (int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+	// WRITE ISPALINDROME METHOD HERE //
+	//                                //
+	//                                //
+	//                                //
+	//                                //
+	////////////////////////////////////
+	public boolean isPalindrome(){
+	    if (head == tail) return true;
+	    Node h = head;
+	    Node t = tail;
+	    for (int i = 0; i < length-1; i++){
+	        if(h.value != t.value) return false;
+	        h = h.next;
+	        t = t.prev;
+	    }
+	    return true;
+	}
+
+}
+
