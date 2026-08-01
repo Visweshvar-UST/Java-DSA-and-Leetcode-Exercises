37588a9628b1544527d4e912b6749df39d776df8
Visweshvar <visweshvar.ust@gmail.com>
2026-08-01 18:56:38 +0530

    Update DoublyLinkedList.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

--- src/DSA/S7/DoublyLinkedList.java
+++ src/DSA/S7/DoublyLinkedList.java
@@ -140,6 +140,26 @@ public class DoublyLinkedList {
         return true;
     }
 
+    public boolean insert(int index, int value) {
+        if(index < 0 || index > length) return false;
+        if(index == 0) {
+            prepend(value);
+            return true;
+        }
+        if(index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node before = get(index - 1);
+        Node after = before.next;
+        newNode.prev = before;
+        newNode.next = after;
+        before.next = newNode;
+        after.prev = newNode;
+        length++;
+        return true;
+    }
 
     public Node removeOwn(int index){
         if (index < 0 || index >= length) return null;
