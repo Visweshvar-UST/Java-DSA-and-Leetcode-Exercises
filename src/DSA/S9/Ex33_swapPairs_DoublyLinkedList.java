956880bda2fe1f18c916c7f345f9569702ed2ed0
Visweshvar <visweshvar.ust@gmail.com>
2026-08-06 14:50:22 +0530

    Update Ex33_swapPairs_DoublyLinkedList.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

--- src/DSA/S9/Ex33_swapPairs_DoublyLinkedList.java
+++ src/DSA/S9/Ex33_swapPairs_DoublyLinkedList.java
@@ -90,6 +90,33 @@ public class Ex33_swapPairs_DoublyLinkedList {
         head.prev = null;
     }
 
+    /// Solution form dis
+    public void swapPairs() {
+        Node dummyNode = new Node(0);
+        dummyNode.next = head;
+        Node previousNode = dummyNode;
 
+        while (head != null && head.next != null) {
+            Node firstNode = head;
+            Node secondNode = head.next;
+
+            previousNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            secondNode.prev = previousNode;
+            firstNode.prev = secondNode;
+
+            if (firstNode.next != null) {
+                firstNode.next.prev = firstNode;
+            }
+
+            head = firstNode.next;
+            previousNode = firstNode;
+        }
+
+        head = dummyNode.next;
+        if (head != null) head.prev = null;
+    }
 }
 
