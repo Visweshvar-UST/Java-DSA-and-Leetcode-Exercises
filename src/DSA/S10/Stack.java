d93a801f9d8a9dff7ac03596ad0eb37a49357fbf
Visweshvar <visweshvar.ust@gmail.com>
2026-08-06 15:08:12 +0530

    Create Stack.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

---
+++ src/DSA/S10/Stack.java
@@ -0,0 +1,36 @@
+package DSA.S10;
+
+public class Stack {
+    public Node top;
+    public int height;
+
+    class Node{
+        public int value;
+        public Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    public Stack(int value){
+        Node newNode = new Node(value);
+        height++;
+    }
+
+    public void printStack(){
+        Node temp = top;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public Node getTop() {
+        return top;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
