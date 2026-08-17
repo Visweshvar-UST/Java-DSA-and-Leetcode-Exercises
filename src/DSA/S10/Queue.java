e83a0d1471b19c3c7a72765627c137a381f0d1fc
Visweshvar <visweshvar.ust@gmail.com>
2026-08-17 20:51:19 +0530

    Update Queue.java
    Co-Authored-By: VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>

--- src/DSA/S10/Queue.java
+++ src/DSA/S10/Queue.java
@@ -51,4 +51,18 @@ public class Queue {
         }
         length++;
     }
+
+    public Node deQueue(){
+        if(length == 0) return null;
+        Node temp = first;
+        if(length == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = first.next;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
 }
